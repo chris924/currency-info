271c5614a5cb6378e8197987a1adaa9d12349b01
chris924 <burkakrisztofer@hotmail.com>
2023-12-19 16:03:29 +0100

    feat: initialize starter code, CMake settings

---
+++ currency-info/currency-info.h
@@ -0,0 +1,10 @@
+﻿// currency-info.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+
+// TODO: Reference additional headers your program requires here.
